a180b9c665752e565e578eb6bcf9bb00a253963f
acamlibe <acamlibe@uncc.edu>
2023-08-05 23:36:12 -0400

    Added moving img directory on npm build

--- move-static-files.js
+++ move-static-files.js
@@ -3,3 +3,7 @@ var fs = require("fs-extra");
 fs.copy('src/fonts', 'dist/fonts', function (err) {
   if (err) return console.error(err)
 });
+
+fs.copy('src/img', 'dist/img', function (err) {
+  if (err) return console.error(err)
+});
