bebf7c2966206c1fb5005d451af6be5755a2c8f9
Ali Camlibel <acamlibe@uncc.edu>
2024-04-13 11:15:55 -0400

    Added tap event listeners

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,5 +1,6 @@
 var content = document.getElementById("main-content");
 var nav = document.getElementById("nav-menu-pages");
+var menuButton = document.getElementById("menu-hamburger");
 
 const toggleMobileNav = () => {
     if (isMobileNavExpanded()) {
@@ -7,31 +8,39 @@ const toggleMobileNav = () => {
     } else {
         expandMobileNav()
     }
-}
+};
 
 const expandMobileNav = () => {
     nav.style.display = "flex";
-}
+};
 
 const collapseMobileNav = () => {
     nav.style.display = "none";
-}
+};
 
 const isMobileNavExpanded = () => {
     return nav.style.display === "flex";
-}
+};
 
-content.onclick = () => {
+const contentClick = () => {
     if (window.innerWidth < 1100) {
         collapseMobileNav();
     }
 };
 
-window.onresize = () => {
+const windowResize = () => {
     if (!isMobileNavExpanded() && window.innerWidth >= 1100) {
         expandMobileNav();
     }
     else if (isMobileNavExpanded() && window.innerWidth < 1100) {
         collapseMobileNav();
     }
-}
+};
+
+menuButton.addEventListener("click", toggleMobileNav);
+menuButton.addEventListener("tap", toggleMobileNav);
+
+content.addEventListener("click", contentClick);
+content.addEventListener("tap", contentClick);
+
+window.addEventListener("resize", windowResize);
